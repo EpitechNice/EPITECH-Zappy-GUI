f1856ca5923380025ab995da26f32524a72b7b84
Aeliondw <caroline.boilly@epitech.eu>
2024-06-05 16:44:07 +0200

    [+] Implement i18n in DisplayCreditScene

--- src/Scenes/CreditScene/DisplayCreditScene/DisplayCreditScene.cpp
+++ src/Scenes/CreditScene/DisplayCreditScene/DisplayCreditScene.cpp
@@ -10,43 +10,55 @@
 namespace Zappy {
     namespace GUI {
         namespace Scene {
+
             CreditScene::CreditScene(std::shared_ptr<Zappy::GUI::Raylib::Render> render, std::string text, int sceneNbr) : _sceneNbr(sceneNbr)
             {
+                Zappy::GUI::I18n::I18nHelper* i18nHelper = Zappy::GUI::I18n::I18nHelper::getInstance();
+
                 _render = render;
                 _background = std::make_unique<Zappy::GUI::Component::Background2D>("assets/img/map_forest_scenery.png");
-                _backButton = std::make_unique<Zappy::GUI::Component::Button>(std::make_pair(20, 20), std::make_pair(-20, -10), "<- BACK", 20, GREEN);
-                _text = std::make_unique<Zappy::GUI::Component::Text>(std::make_pair(_render->getWidth() / 3 , _render->getHeight() / 3), text, 30, WHITE);
-            }
-
-            void CreditScene::destroy()
-            {
-                _text->destroy();
-                _backButton->destroy();
-                _background->destroy();
+                _backButton = std::make_pair(std::make_unique<Zappy::GUI::Component::Button>(std::make_pair(20, 20), std::make_pair(-20, -10), i18nHelper->getTranslation("[menu.back_button]"), 20, GREEN), "[menu.back_button]");
+                _text = std::make_pair(std::make_unique<Zappy::GUI::Component::Text>(std::make_pair(_render->getWidth() / 3 , _render->getHeight() / 3), i18nHelper->getTranslation(text), 30, WHITE), text);
+                _lang = i18nHelper->getCurrentLocale();
             }
 
             void CreditScene::start()
+            {}
+
+            void CreditScene::destroy()
             {
+                _text.first->destroy();
+                _backButton.first->destroy();
+                _background->destroy();
+            }
+
+            void CreditScene::update()
+            {
+                Zappy::GUI::I18n::I18nHelper* i18nHelper = Zappy::GUI::I18n::I18nHelper::getInstance();
+
+                if (i18nHelper->getCurrentLocale() != _lang) {
+                    _backButton.first->setText(i18nHelper->getTranslation(_backButton.second));
+                    _text.first->setText(i18nHelper->getTranslation(_text.second));
+                    _lang = i18nHelper->getCurrentLocale();
+                }
             }
 
             void CreditScene::event()
-            {
-            }
+            {}
 
             void CreditScene::draw3D()
-            {
-            }
+            {}
 
             void CreditScene::draw2D()
             {
                 _background->draw();
-                _backButton->draw();
-                _text->draw();
+                _backButton.first->draw();
+                _text.first->draw();
             }
 
             std::string CreditScene::nextScene()
             {
-                if (_backButton->isClicked(_backButton->getText()))
+                if (_backButton.first->isClicked(_backButton.second))
                     return "credits";
                 std::string txt = "credit" + std::to_string(_sceneNbr);
                 return txt;
