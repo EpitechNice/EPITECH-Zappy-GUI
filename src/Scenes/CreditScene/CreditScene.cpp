052c20227e6d68278693859d06832a416bf05794
Aeliondw <caroline.boilly@epitech.eu>
2024-06-05 16:21:41 +0200

    [+] Implement i18n in CreditScene

--- src/Scenes/CreditScene/CreditScene.cpp
+++ src/Scenes/CreditScene/CreditScene.cpp
@@ -10,65 +10,78 @@
 namespace Zappy {
     namespace GUI {
         namespace Scene {
+//TODO (Refacto i18n) : Create translation value for"Credit1", "Credit2", "Credit3"
             Credit::Credit(std::shared_ptr<Zappy::GUI::Raylib::Render> render)
             {
+                Zappy::GUI::I18n::I18nHelper* i18nHelper = Zappy::GUI::I18n::I18nHelper::getInstance();
+
                 _render = render;
                 _background = std::make_unique<Zappy::GUI::Component::Background2D>("assets/img/map_forest_scenery.png");
-                _backButton = std::make_unique<Zappy::GUI::Component::Button>(std::make_pair(20, 20), std::make_pair(-20, -10), "<- BACK", 20, GREEN);
+                _backButton = std::make_pair(std::make_unique<Zappy::GUI::Component::Button>(std::make_pair(20, 20), std::make_pair(-20, -10), i18nHelper->getTranslation("[menu.back_button]"), 20, GREEN), "[menu.back_button]");
                 int x_generalButton = GetScreenWidth() / 2;
                 int y_generalButton = 0;
                 int i_generalButton = 0;
                 int height = GetScreenHeight();
-                std::vector<std::pair<std::string, std::string>> buttons = {
-                    {"Credit1", "credit1"},
-                    {"Credit2", "credit2"},
-                    {"Credit3", "credit3"},
+                std::vector<std::tuple<std::string, std::string, std::string>> buttons = {
+                    {i18nHelper->getTranslation("[menu.settings.credits.1_button]"), "credit1", "[menu.settings.credits.1_button]"},
+                    {i18nHelper->getTranslation("[menu.settings.credits.2_button]"), "credit2", "[menu.settings.credits.2_button]"},
+                    {i18nHelper->getTranslation("[menu.settings.credits.3_button]"), "credit3", "[menu.settings.credits.3_button]"},
                 };
+                _lang = i18nHelper->getCurrentLocale();
                 for (auto &button : buttons) {
-                    auto button_width = std::make_unique<Zappy::GUI::Component::Button>(std::make_pair(0, 0), std::make_pair(-20, -10), button.first, 30, GREEN)->getSize().first;
+                    auto button_width = std::make_unique<Zappy::GUI::Component::Button>(std::make_pair(0, 0), std::make_pair(-20, -10), std::get<0>(button), 30, GREEN)->getSize().first;
                     y_generalButton = (height / (buttons.size() + 1)) * (i_generalButton + 1) - 30;
                     x_generalButton -= button_width / 2;
-                    _generalButtons.push_back(std::make_pair(std::make_unique<Zappy::GUI::Component::Button>(std::make_pair(x_generalButton, y_generalButton), std::make_pair(-20, -10), button.first, 30, GREEN), button.second));
+                    _generalButtons.push_back(std::make_tuple(std::make_unique<Zappy::GUI::Component::Button>(std::make_pair(x_generalButton, y_generalButton), std::make_pair(-20, -10), std::get<0>(button), 30, GREEN), std::get<1>(button), std::get<2>(button)));
                     x_generalButton += button_width / 2;
                     i_generalButton++;
                 }
             }
 
+            void Credit::start()
+            {}
+
             void Credit::destroy()
             {
                 for (auto &button : _generalButtons)
-                    button.first->destroy();
+                    std::get<0>(button)->destroy();
                 _generalButtons.clear();
-                _backButton->destroy();
+                std::get<0>(_backButton)->destroy();
                 _background->destroy();
             }
 
-            void Credit::start()
+            void Credit::update()
             {
+                Zappy::GUI::I18n::I18nHelper* i18nHelper = Zappy::GUI::I18n::I18nHelper::getInstance();
+
+                if (i18nHelper->getCurrentLocale() != _lang) {
+                    for (auto &button : _generalButtons)
+                        std::get<0>(button)->setText(i18nHelper->getTranslation(std::get<2>(button)));
+                    _backButton.first->setText(i18nHelper->getTranslation(_backButton.second));
+                    _lang = i18nHelper->getCurrentLocale();
+                }
             }
 
             void Credit::event()
-            {
-            }
+            {}
 
             void Credit::draw3D()
-            {
-            }
+            {}
 
             void Credit::draw2D()
             {
                 _background->draw();
-                _backButton->draw();
+                _backButton.first->draw();
                 for (auto &button : _generalButtons)
-                    button.first->draw();
+                    std::get<0>(button)->draw();
             }
 
             std::string Credit::nextScene()
             {
                 for (auto &button : _generalButtons) {
-                    if (button.first->isClicked(button.first->getText()))
-                        return button.second;
-                    if (_backButton->isClicked(_backButton->getText()))
+                    if (std::get<0>(button)->isClicked(std::get<2>(button)))
+                        return std::get<1>(button);
+                    if (_backButton.first->isClicked(_backButton.second))
                         return "menu";
                 }
                 return "credits";
