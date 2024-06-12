0a957a94b3aebbe8b63bc024cb9816f02aa21f8a
dragusheen <nathan.tirolf@epitech.eu>
2024-06-12 13:44:03 +0200

    [~] Clean ComponentButtonClassic

--- src/Components/ButtonClassic/ComponentButtonClassic.cpp
+++ src/Components/ButtonClassic/ComponentButtonClassic.cpp
@@ -11,37 +11,39 @@ namespace Zappy {
     namespace GUI {
         namespace Component {
             ButtonClassic::ButtonClassic(std::pair<float, float> pos, std::pair<float, float> size, std::string text, int textSize, Color color)
-                : _pos(pos), _size(size), _color(color), _state(DEFAULT), _textStrokeVisible(false), _stateEnabled(true), _isDestroyed(false)
             {
+                _posX = pos.first;
+                _posY = pos.second;
+                _sizeX = size.first;
+                _sizeY = size.second;
+                _color = color;
+                _state = DEFAULT;
+                _textStrokeVisible = false;
+                _stateEnabled = true;
+
+
                 _text = std::make_unique<Text>(std::make_pair(pos.first, pos.second), text, textSize, WHITE);
                 std::pair<float, float> textSizes = {_text->getSizeX(), _text->getSizeY()};
-                if (_size.first < 0)
-                    _size.first = (_size.first * -1) * 2 + textSizes.first;
-                if (_size.second < 0)
-                    _size.second = (_size.second * -1) * 2 + textSizes.second;
-                _textPos = std::make_pair(pos.first + (_size.first - textSizes.first) / 2, pos.second + (_size.second - textSizes.second) / 2);
+                if (_sizeX < 0)
+                    _sizeX = (_sizeX * -1) * 2 + textSizes.first;
+                if (_sizeY < 0)
+                    _sizeY = (_sizeY * -1) * 2 + textSizes.second;
 
-                _text->setPosX(_textPos.first);
-                _text->setPosY(_textPos.second);
+                _text->setPosX(pos.first + (_sizeX - textSizes.first) / 2);
+                _text->setPosY(pos.second + (_sizeY - textSizes.second) / 2);
                 _state = DEFAULT;
                 _button = std::make_unique<RoundedRectangle>(pos, size, 0.3, _color);
-                _textStroke = std::make_unique<Text>(std::make_pair(_textPos.first, _textPos.second + 2), text, textSize, Zappy::GUI::Raylib::ColorManager::Darker(color, 50));
+                _textStroke = std::make_unique<Text>(std::make_pair(_text->getPosX(), _text->getPosY() + 2), text, textSize, Zappy::GUI::Raylib::ColorManager::Darker(color, 50));
 
                 _lastClick = -1;
             }
 
-            ButtonClassic::~ButtonClassic()
-            {
-                destroy();
-            }
-
             void ButtonClassic::destroy()
             {
-                if (!_isDestroyed) {
-                    _text->destroy();
-                    _textStroke->destroy();
-                    _isDestroyed = true;
-                }
+                if (_isDestroyed) return;
+                _text->destroy();
+                _textStroke->destroy();
+                _isDestroyed = true;
             }
 
             void ButtonClassic::draw()
@@ -56,42 +58,49 @@ namespace Zappy {
                 _text->draw();
             }
 
-            std::pair<float, float> ButtonClassic::getSize() const
-            {
-                return _size;
-            }
 
-            bool ButtonClassic::isClicked() const
+
+            void ButtonClassic::setPosX(float x)
             {
-                return _state == CLICKED;
+                float tmp = _posX;
+                tmp -= x;
+                _posX = x;
+                _button->setPosX(x);
+                _text->modPosX(-tmp);
+                _textStroke->modPosX(-tmp);
             }
 
             void ButtonClassic::modPosX(float x)
             {
-                setPosX(_pos.first + x);
+                setPosX(_posX + x);
             }
 
-            void ButtonClassic::setPosX(float x)
+            void ButtonClassic::setPosY(float y)
             {
-                setPos(std::make_pair(x, _pos.second));
+                float tmp = _posY;
+                tmp -= y;
+                _posY = y;
+                _button->setPosY(y);
+                _text->modPosY(-tmp);
+                _textStroke->modPosY(-tmp + 2);
             }
 
-            void ButtonClassic::setPos(std::pair<float, float> pos)
+            void ButtonClassic::modPosY(float y)
             {
-                std::pair<float, float> tmp = _pos;
-                tmp.first -= pos.first;
-                tmp.second -= pos.second;
-                _pos = pos;
-                _textPos.first -= tmp.first;
-                _textPos.second -= tmp.second;
+                setPosY(_posY + y);
+            }
 
-                _button->setPosX(pos.first);
-                _button->setPosY(pos.second);
+            void ButtonClassic::setColor(Color color)
+            {
+                _color = color;
+                _button->setColor(color);
+                _textStroke->setColor(Zappy::GUI::Raylib::ColorManager::Darker(color, 50));
+            }
 
-                _text->setPosX(_textPos.first);
-                _text->setPosY(_textPos.second);
-                _textStroke->setPosX(_textPos.first);
-                _textStroke->setPosY(_textPos.second + 2);
+
+            bool ButtonClassic::isClicked() const
+            {
+                return _state == CLICKED;
             }
 
             void ButtonClassic::enableTextStroke()
@@ -116,19 +125,12 @@ namespace Zappy {
                 _state = NONE;
             }
 
-            void ButtonClassic::setColor(Color color)
-            {
-                _color = color;
-                _button->setColor(color);
-                _textStroke->setColor(Zappy::GUI::Raylib::ColorManager::Darker(color, 50));
-            }
-
 
             void ButtonClassic::_updateState()
             {
                 if (!_stateEnabled)
                     return;
-                if (CheckCollisionPointRec(GetMousePosition(), Rectangle{_pos.first, _pos.second, _size.first, _size.second})) {
+                if (CheckCollisionPointRec(GetMousePosition(), Rectangle{_posX, _posY, _sizeX, _sizeY})) {
                     if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && time(nullptr) > _lastClick + 0.5) {
                         _state = CLICKED;
                         _lastClick = time(nullptr);
