5e50b84aecf4b462fee86e47c4cbb13cda55676b
dragusheen <nathan.tirolf@epitech.eu>
2024-06-12 19:15:16 +0200

    [~] Clean Helper Scene - add new map scenery (war) set it on helper and credits

--- src/Scenes/HelperScene/HelperScene.hpp
+++ src/Scenes/HelperScene/HelperScene.hpp
@@ -14,6 +14,7 @@
     #include "AScene.hpp"
     #include "ComponentBackground2D.hpp"
     #include "ComponentButton.hpp"
+    #include "ComponentTextBox.hpp"
 
 namespace Zappy {
     namespace GUI {
@@ -23,10 +24,8 @@ namespace Zappy {
                     HelperScene(std::shared_ptr<Zappy::GUI::Raylib::Render> render, std::string text);
                     HelperScene() = default;
 
-                    void start() override;
                     void destroy() override;
                     void update() override;
-                    void draw3D() override;
                     void draw2D() override;
 
                     std::string nextScene() override;
@@ -35,7 +34,7 @@ namespace Zappy {
                 private:
                     std::unique_ptr<Zappy::GUI::Component::Background2D> _background;
                     std::pair<std::unique_ptr<Zappy::GUI::Component::Button>, std::string> _backButton;
-                    std::pair<std::unique_ptr<Zappy::GUI::Component::Text>, std::string> _text;
+                    std::pair<std::unique_ptr<Zappy::GUI::Component::TextBox>, std::string> _text;
                     std::shared_ptr<Zappy::GUI::Raylib::Render> _render;
                     Zappy::GUI::I18n::SupportedLocale _lang;
             };
