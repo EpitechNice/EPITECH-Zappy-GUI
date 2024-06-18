9193d9845dad172a133448f64176d341e150594e
dragusheen <nathan.tirolf@epitech.eu>
2024-06-18 14:43:34 +0200

    [+] Resize & Doc on Model3D (nedd to redo when arrive on component that callm model3D)

--- src/Components/Model3D/ComponentModel3D.cpp
+++ src/Components/Model3D/ComponentModel3D.cpp
@@ -18,19 +18,27 @@ namespace Zappy {
                 _scale = scale;
                 _rotationAxis = rotationAxis;
                 _angle = angle;
+                _path = path;
 
                 _model = Raylib::ModelManager::get()->getModel(path);
             }
 
             void Model3D::draw()
             {
-                DrawModelEx(_model, { _posX, _posY, _posZ }, _rotationAxis, _angle, { _scale, _scale, _scale }, WHITE);
+                // DrawModelEx(_model, { _posX, _posY, _posZ }, _rotationAxis, _angle, { _scale, _scale, _scale }, WHITE);
+                DrawModelEx(Raylib::ModelManager::get()->getModel(_path), { _posX, _posY, _posZ }, _rotationAxis, _angle, { _scale, _scale, _scale }, WHITE);
+            }
+
+            void Model3D::resize()
+            {
+                _model = Raylib::ModelManager::get()->getModel(_path);
             }
 
 
             void Model3D::setOnPosY(float y)
             {
-                BoundingBox box = GetModelBoundingBox(_model);
+                // BoundingBox box = GetModelBoundingBox(_model);
+                BoundingBox box = GetModelBoundingBox(Raylib::ModelManager::get()->getModel(_path));
                 _posY =  y + (box.max.y - box.min.y) / 2 * _scale;
             }
         }
