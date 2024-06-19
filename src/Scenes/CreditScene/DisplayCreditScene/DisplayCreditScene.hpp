48df38bd653834f1d974834d44775dfbd5a3011b
dragusheen <nathan.tirolf@epitech.eu>
2024-06-19 12:03:03 +0200

    [+] Dox on Scenes

--- src/Scenes/CreditScene/DisplayCreditScene/DisplayCreditScene.hpp
+++ src/Scenes/CreditScene/DisplayCreditScene/DisplayCreditScene.hpp
@@ -19,16 +19,47 @@
 namespace Zappy {
     namespace GUI {
         namespace Scene {
+            /**
+             * @brief CreditScene class
+             */
             class CreditScene : public AScene {
                 public:
+                    /**
+                     * @brief CreditScene constructor
+                     *
+                     * @param render The render
+                     * @param text The text
+                     * @param sceneNbr The scene number
+                     */
                     CreditScene(std::shared_ptr<Zappy::GUI::Raylib::Render> render, std::string text, int sceneNbr);
-                    CreditScene() = default;
 
+                    /**
+                     * @brief Destroy the CreditScene object
+                     *
+                     * @warning After the destruction of the CreditScene object, you won't be able to use it anymore
+                     */
                     void destroy() override;
+
+                    /**
+                     * @brief Update the CreditScene object
+                     */
                     void update() override;
+
+                    /**
+                     * @brief Draw the 2D objects of the CreditScene object
+                     */
                     void draw2D() override;
+
+                    /**
+                     * @brief Resize the CreditScene object
+                     */
                     void resize() override;
 
+                    /**
+                     * @brief Get the next scene
+                     *
+                     * @return `std::string` The next scene
+                     */
                     std::string nextScene() override;
 
                 protected:
